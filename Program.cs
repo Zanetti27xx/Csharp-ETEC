c3238698e3367b1e643fef0cf3459a1daa730150
Zanetti27xx <Black777progamer@gmail.com>
2025-02-18 17:21:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lampada3
+{
+    class Lampada
+    {
+        public int lampadaNro;
+        public string comodo;
+        public bool status;
+
+        public void provarExistencia()
+        {
+            Console.WriteLine("Eu existo!!!");
+        }
+
+        static void Main(string[] args)
+        {
+            Lampada l1 = new Lampada();
+            l1.provarExistencia();
+            l1.lampadaNro = 1;
+            l1.comodo = "Sala";
+            l1.status = true;
+
+            Lampada l2 = new Lampada();
+            l2.provarExistencia();
+            l2.lampadaNro = 2;
+            l2.comodo = "Quarto";
+            l2.status = false;
+        }
+    }
+}
