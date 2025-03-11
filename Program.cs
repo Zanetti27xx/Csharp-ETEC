e78c449f32e4bcbc64b514fda785e160bdb69be1
Zanetti27xx <Black777progamer@gmail.com>
2025-03-11 15:18:08 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,79 @@
 ﻿using System;
 
-namespace Lampada3
+namespace Banco
 {
-    class Lampada
+    class Banco
     {
-        public int lampadaNro;
-        public string comodo;
-        public bool status;
+        string cpf;
+        string nome;
+        int limiteCredito;
+        float saldo;
+        bool negativado;
+        float valorDep;
+        float valorSac;
 
-        public void provarExistencia()
+        public void depositar()
         {
-            Console.WriteLine("Eu existo!!!");
+            Console.WriteLine("Quanto deseja depositar?");
+            float valorDep = float.Parse(Console.ReadLine());
+            saldo = saldo + valorDep;
+            Console.WriteLine("Valor Depositado!\n\n");
+            confirmar();
+        }
+        public void sacar()
+        {
+            Console.WriteLine("Quanto deseja sacar?");
+            float valorSac = float.Parse(Console.ReadLine());
+            saldo = saldo - valorSac;
+            Console.WriteLine("Valor Sacado!\n\n");
+            confirmar();
         }
 
+        public void consulta()
+        {
+            Console.WriteLine("nome: " + nome);
+            Console.WriteLine("cpf: " + cpf);
+            Console.WriteLine("limite de credito: " + limiteCredito);
+            Console.WriteLine("saldo: " + saldo);
+            if(negativado == false)
+            {
+                Console.WriteLine("negativado: Não\n\n");
+            } else
+            {
+                Console.WriteLine("negativado: Sim\n\n");
+            }
+            confirmar();
+        }
+
+        public void confirmar()
+        {
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1: Consultar");
+            Console.WriteLine("2: Depositar");
+            Console.WriteLine("3: Sacar");
+            string confirmar = Console.ReadLine();
+            if (confirmar == "1")
+            {
+                consulta();
+            }
+            if (confirmar == "2")
+            {
+                depositar();
+            }
+            if (confirmar == "3")
+            {
+                sacar();
+            }
+        }
         static void Main(string[] args)
         {
-            Lampada l1 = new Lampada();
-            l1.provarExistencia();
-            l1.lampadaNro = 1;
-            l1.comodo = "Sala";
-            l1.status = true;
-
-            Lampada l2 = new Lampada();
-            l2.provarExistencia();
-            l2.lampadaNro = 2;
-            l2.comodo = "Quarto";
-            l2.status = false;
+            Banco p1 = new Banco();
+            p1.cpf = "38838838892";
+            p1.nome = "Matheus";
+            p1.limiteCredito = 1200;
+            p1.saldo = 500.00f;
+            p1.negativado = false;
+            p1.confirmar();
+            }
         }
     }
-}
